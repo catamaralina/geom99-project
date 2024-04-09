34d8517a396b56efe72eeeb0b2db5c200bc49fd6
Andie <146446987+aherstek@users.noreply.github.com>
2024-04-09 15:07:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -14,20 +14,4 @@ require(["esri/config", "esri/Map", "esri/views/MapView"], function(esriConfig, 
         zoom: 13, // Zoom level
         container: "viewDiv" // Div element
     });
-
-    // Slideshow for home page 
-    var slideIndex = 0;
-    showSlides();
-
-    function showSlides() {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";  
-        }
-        slideIndex++;
-        if (slideIndex > slides.length) {slideIndex = 1}    
-        slides[slideIndex-1].style.display = "block";  
-        setTimeout(showSlides, 2000); // Change image every 2 seconds?
-    }
 });
