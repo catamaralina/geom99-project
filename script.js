960b030edcd4c44b7f5b69ff40bf2b99b0a950d7
Andie <146446987+aherstek@users.noreply.github.com>
2024-04-09 14:37:35 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 require(["esri/config", "esri/Map", "esri/views/MapView"], function(esriConfig, Map, MapView) {
-  esriConfig.apiKey = "API Key";
+    esriConfig.apiKey = "API Key";
 
-  const webmap = new WebMap({
-    portalItem: {
-      // autocasts as new PortalItem()
-      id: "App ID"
+    const webmap = new WebMap({
+        portalItem: {
+            // autocasts as new PortalItem()
+            id: "App ID"
+        }
+    });
+    
+    const view = new MapView({
+        map: Map,
+        center: [X, Y], // Longitude, latitude
+        zoom: 13, // Zoom level
+        container: "viewDiv" // Div element
+    });
+
+    // Slideshow for home page 
+    var slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}    
+        slides[slideIndex-1].style.display = "block";  
+        setTimeout(showSlides, 2000); // Change image every 2 seconds?
     }
-  });
-  const view = new MapView({
-    map: Map,
-    center: [X,Y], // Longitude, latitude
-    zoom: 13, // Zoom level
-    container: "viewDiv" // Div element
-  });
 });
